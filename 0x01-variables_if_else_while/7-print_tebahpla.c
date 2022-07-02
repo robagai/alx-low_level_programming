81443b0ceec1eb4d2a26614f1e6b1bf444c2304d
robagai <robagai@gmail.com>
2022-07-02 16:23:58 -0700

    m

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,11 +10,12 @@
 int main(void)
 {
 	char c = 'z';
-	
+
 	while (c >= 'a')
 	{
 		putchar(c);
 		c--;
-	}putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int i, y;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-		putchar (i + '0'); 
+		putchar (i + '0');
 	}
 	for (y = 'a'; y <= 'f'; y++)
 	{
