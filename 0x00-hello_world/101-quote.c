cd1f4fdb098c10754cdbdef1e24810aa0f85fd50
robagai <robagai@gmail.com>
2022-07-03 14:48:11 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>                                                                                                                
-                                                                                                                                  
-                                                                        
-                                                                                          
-/**                                                    
- * main - prints to string
- *Description: Prints "and that piece of art is useful.." without puts
- * Return: 1
- */
+#include <stdio.h>
 
+
+
+/**
+* main - prints to string
+* Description: Prints "and that piece of art is useful.." without puts
+* Return: 1
+*/
 int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
