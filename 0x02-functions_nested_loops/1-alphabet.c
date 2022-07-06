2a1e13554e9c6aaca22c8e3b673bacbb528d54e3
robagai <robagai@gmail.com>
2022-07-06 12:17:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-* print_alphabet - print the alphabet
-*/
+ * print_alphabet - print the alphabet
+ */
 void print_alphabet(void)
 {
-	char a = 'a';
+	char a='a';
 
 	while (a <= 'z')
 	{
 		_putchar(a);
 		++a;
 	}
+
 	_putchar(10);
 }
+	
