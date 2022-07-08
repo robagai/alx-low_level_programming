322364883166f7ff738a0d2310b0f009788fd293
robagai <robagai@gmail.com>
2022-07-08 07:32:56 -0700

    Task 1-11

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Description: multiplies 2 integers
+ * Return: the result of the operation
+ */
+
+int mul(int a, int b)
+
+{
+
+	int c;
+
+
+
+	c = a * b;
+
+
+
+	return (c);
+
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ * Description: prints the numbers, from 0 to 9
+ */
+
+void print_numbers(void)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < 10; i++)
+
+	{
+
+		_putchar(i + '0');
+
+	}
+
+
+
+	_putchar('\n');
+
+}
