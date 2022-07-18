4c4d7d694e99ef812c73c4fbc32eb0bf6b8b75b6
robagai <robagai@gmail.com>
2022-07-18 02:50:36 -0700

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,29 +1,15 @@
-#include "main.h"
-/**
- *_strcat - concatenates two strings
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will also be changed
- *Return: dest
- */
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-char *_strcat(char *dest, char *src)
-{
-int i, j;
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
-}
+#endif}
