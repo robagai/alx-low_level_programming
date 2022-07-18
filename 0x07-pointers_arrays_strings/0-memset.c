9c51d30f194c49c5c3fbecbdddf13aa73ea9cccd
robagai <robagai@gmail.com>
2022-07-18 02:52:43 -0700

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,20 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include "main.h"
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-int _putchar(char c);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+/**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
 
-#endif}
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
